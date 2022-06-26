e4852bee8b86e6b65011082171e57f277e12c49f
Nehemiah Dauda <105819615+MegaNAD60@users.noreply.github.com>
2022-06-26 17:05:04 +0100

    Add files via upload

---
+++ Calculator/myScript.js
@@ -0,0 +1,4 @@
+/*----------functionality of calculator codes----------*/
+function input(val){document.getElementById("display").value+=val;}
+function cleared(){document.getElementById("display").value="";}
+function cal()  {calculator.display.value = eval(calculator.display.value);}
